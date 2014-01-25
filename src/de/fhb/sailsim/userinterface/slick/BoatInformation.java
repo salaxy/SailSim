a6fe46825162c63ccef7caf5c7194a5ba22056a8
Salaxy <salaxy@web.de>
2014-01-25 17:19:57 +0100

    - finalize BoatInformation.java

--- src/de/fhb/sailsim/userinterface/slick/BoatInformation.java
+++ src/de/fhb/sailsim/userinterface/slick/BoatInformation.java
@@ -34,15 +34,23 @@ public class BoatInformation {
 		DecimalFormatSymbols dfs = DecimalFormatSymbols.getInstance();
 		dfs.setDecimalSeparator('.');
 		DecimalFormat f = new DecimalFormat("#0.00", dfs);
-		
+
 		graphics.drawString(
-				"Boat proV: " + f.format(this.boatState.getCurrentPropulsionVelocity()),
-				X_POS, Y_POS);
-		graphics.drawString(
-				"Boat spinV: " + f.format(this.boatState.getCurrentSpinVelocity()),
+				"Boat proV: " + f.format(this.boatState.getCurrentPropulsionVelocity())+" m/s", X_POS,
+				Y_POS);
+		graphics.drawString("Boat spinV: " + f.format(this.boatState.getCurrentSpinVelocity())+" °/s",
 				X_POS, Y_POS + 20);
 		// graphics.drawString("Boat proA: " + this.windState.getWindToBoat(),
 		// X_POS, Y_POS + 40);
+
+		graphics.setLineWidth(3);
+		int measureLine = 60;
+		int measureLength = 100;
+		graphics.drawString("Measure 100 m", X_POS, Y_POS + measureLine+5);
+		graphics.drawLine(X_POS, Y_POS + measureLine, X_POS + 100, Y_POS + measureLine);
+		graphics.drawLine(X_POS, Y_POS + measureLine - 5, X_POS, Y_POS + measureLine + 5);
+		graphics.drawLine(X_POS + measureLength, Y_POS + measureLine - 5, X_POS + measureLength,
+				Y_POS + measureLine + 5);
 
 		graphics.resetTransform();
 	}
